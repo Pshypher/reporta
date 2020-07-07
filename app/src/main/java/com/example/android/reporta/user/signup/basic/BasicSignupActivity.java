ed24e7abd60499d005c2952ced801a5d0bef78b6
Shote Babajimi <jimishote@live.com>
2020-07-07 14:56:35 +0100

    Completed design of signup user interface

---
+++ app/src/main/java/com/example/android/reporta/user/signup/basic/BasicSignupActivity.java
@@ -0,0 +1,16 @@
+package com.example.android.reporta.user.signup.basic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.android.reporta.R;
+
+public class BasicSignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basic_signup);
+    }
+}
