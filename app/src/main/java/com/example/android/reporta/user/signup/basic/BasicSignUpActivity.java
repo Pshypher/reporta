daff24575f6055197063ca34ccb869b10d66d2dd
larmie56 <adeleyeolamide56@gmail.com>
2020-08-10 04:34:47 +0100

    Implement Facebook authentication

--- app/src/main/java/com/example/android/reporta/user/signup/basic/BasicSignUpActivity.java
+++ app/src/main/java/com/example/android/reporta/user/signup/basic/BasicSignUpActivity.java
@@ -1,5 +1,7 @@
 package com.example.android.reporta.user.signup.basic;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -15,9 +17,26 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.android.reporta.R;
+import com.example.android.reporta.firebase_utils.FirebaseUtils;
 import com.example.android.reporta.user.signup.detailed.DetailedSignUpActivity;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.firebase.ui.auth.AuthMethodPickerLayout;
 import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthCredential;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class BasicSignUpActivity extends AppCompatActivity {
     public static String EMAIL_EXTRA = "BasicSignUpActivity.Email";
@@ -34,6 +53,11 @@ public class BasicSignUpActivity extends AppCompatActivity {
     private EditText mEditTextEmailAddress;
     private EditText mEditTextPassword;
     private Button mContinueButton;
+    private LoginButton mLoginButton;
+
+    //Facebook callback
+    private CallbackManager mCallbackManager;
+    private FirebaseAuth mFirebaseAuth;
 
     private String mEmailAddress;
     private String mPassword;
@@ -48,8 +72,8 @@ public class BasicSignUpActivity extends AppCompatActivity {
         initFields();
         verifyUserInput();
         continueSignUp();
-        handleGoogleSignUp();
-        //TODO: handleFacebookSignUp
+        //TODO: properly handleGoogleSignUp();
+        handleFacebookSignUp();
     }
 
     private void continueSignUp() {
@@ -130,22 +154,80 @@ public class BasicSignUpActivity extends AppCompatActivity {
         googleButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                AuthMethodPickerLayout basicSignUpLayout = new AuthMethodPickerLayout
+                AuthMethodPickerLayout googleSignUp = new AuthMethodPickerLayout
                         .Builder(R.layout.activity_basic_signup)
                         .setGoogleButtonId(R.id.logo_google)
                         .build();
 
                 startActivityForResult(
                         AuthUI.getInstance().createSignInIntentBuilder()
-                                .setAuthMethodPickerLayout(basicSignUpLayout)
+                                .setAuthMethodPickerLayout(googleSignUp)
                                 .build(), RC_SIGN_IN);
             }
         });
+
+        ImageView facebookButton = findViewById(R.id.login_facebook);
+        facebookButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AuthMethodPickerLayout facebookSignUp = new AuthMethodPickerLayout
+                        .Builder(R.layout.activity_basic_signup)
+                        .setFacebookButtonId(R.id.login_facebook)
+                        .build();
+
+                startActivityForResult(
+                        AuthUI.getInstance().createSignInIntentBuilder()
+                                .setAuthMethodPickerLayout(facebookSignUp)
+                                .build(), RC_SIGN_IN);
+            }
+        });
+    }
+
+    private void handleFacebookSignUp() {
+        mLoginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                AuthCredential credential =
+                        FacebookAuthProvider.getCredential(loginResult.getAccessToken().getToken());
+                mFirebaseAuth.signInWithCredential(credential).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        Toast.makeText(BasicSignUpActivity.this, "User Created!", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(BasicSignUpActivity.this, DetailedSignUpActivity.class));
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(BasicSignUpActivity.this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(BasicSignUpActivity.this, "Cancelled", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Toast.makeText(BasicSignUpActivity.this, "Error: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     private void initFields() {
         mEditTextEmailAddress = findViewById(R.id.email_address);
         mEditTextPassword = findViewById(R.id.password);
         mContinueButton = findViewById(R.id.btn_continue);
+        mLoginButton = findViewById(R.id.login_facebook);
+        mCallbackManager = CallbackManager.Factory.create();
+        mFirebaseAuth = FirebaseUtils.getFirebaseAuth();
     }
 }
