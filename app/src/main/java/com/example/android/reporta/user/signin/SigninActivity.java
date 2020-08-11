0cd09298362fee84551f1af7e526ff74ae9846d7
Shote Babajimi <jimishote@live.com>
2020-08-11 17:16:10 +0100

    Added template for the Signin Activity

---
+++ app/src/main/java/com/example/android/reporta/user/signin/SigninActivity.java
@@ -0,0 +1,16 @@
+package com.example.android.reporta.user.signin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.android.reporta.R;
+
+public class SigninActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+    }
+}
